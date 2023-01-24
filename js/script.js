6f7b0f85ccc5c37c3d9cb60033cc7ac97d455efb
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-24 13:44:39 +0100

    generazione mail

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK');
+
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            mailList: [],
+        }
+    },
+
+    methods: {
+
+        generateAddress() {
+
+            for (let i = 0; i < 10; i++) {
+                this.generateAddress();
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    this.mailList.push(response.data.response)
+                    console.log(this.mailList);
+                })
+
+            }
+        }
+
+    }
+}).mount('#app');
